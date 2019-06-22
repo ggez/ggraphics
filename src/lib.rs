28dc72179a347e038875c1d326b64acc8279d826
Simon Heath <icefox@dreamquest.io>
2019-06-22 09:32:42 -0400

    Refactored setup into its own function
    Pretty raw but it's a start.

--- src/lib.rs
+++ src/lib.rs
@@ -1041,7 +1041,7 @@ loop {
 }
 
 
-pub struct Draw {
+pub struct DrawCall {
     // Mesh, texture, sampler info instance data.
 }
 
@@ -1063,3 +1063,235 @@ impl GraphicsDevice {
 
 pub fn present() {}
 */
+
+/// An initialized graphics device context,
+/// with a window.  The window itself should
+/// be made optional later.  We need it for
+/// building a PresentNode but should be able
+/// to manage without it and add one later.
+pub struct GraphicsDevice<B>
+where
+    B: hal::Backend,
+{
+    // gfx-hal types
+    pub factory: Factory<B>,
+    pub queue_id: QueueId,
+    pub families: rendy::command::Families<B>,
+
+    // Rendy types
+    pub depth: rendy::graph::ImageId,
+    pub color: rendy::graph::ImageId,
+    pub graph: rendy::graph::Graph<B, Aux<B>>,
+    pub aux: Aux<B>,
+    // winit types
+    pub window: winit::Window,
+    pub event_loop: winit::EventsLoop,
+    // Config data
+    pub frame_count: u32,
+    pub align: u64,
+}
+
+impl<B> GraphicsDevice<B>
+where
+    B: hal::Backend,
+{
+    pub fn new() -> Self {
+        use rendy::factory::Config;
+        use rendy::graph::{present::PresentNode, render::*, GraphBuilder};
+        use rendy::hal::PhysicalDevice as _;
+        use winit::{EventsLoop, WindowBuilder};
+
+        let config: Config = Default::default();
+
+        let (mut factory, mut families): (Factory<B>, _) = rendy::factory::init(config).unwrap();
+
+        let mut event_loop = EventsLoop::new();
+
+        let window = WindowBuilder::new()
+            .with_title("Rendy example")
+            .build(&event_loop)
+            .unwrap();
+        let window_size = window
+            .get_inner_size()
+            .unwrap()
+            .to_physical(window.get_hidpi_factor());
+
+        event_loop.poll_events(|_| ());
+
+        let surface = factory.create_surface(&window);
+
+        let mut graph_builder = GraphBuilder::<B, Aux<B>>::new();
+
+        let size = window
+            .get_inner_size()
+            .unwrap()
+            .to_physical(window.get_hidpi_factor());
+
+        let window_kind = hal::image::Kind::D2(size.width as u32, size.height as u32, 1, 1);
+        let color = graph_builder.create_image(
+            window_kind,
+            1,
+            factory.get_surface_format(&surface),
+            Some(hal::command::ClearValue::Color([0.1, 0.2, 0.3, 1.0].into())),
+        );
+
+        let depth = graph_builder.create_image(
+            window_kind,
+            1,
+            hal::format::Format::D16Unorm,
+            Some(hal::command::ClearValue::DepthStencil(
+                hal::command::ClearDepthStencil(1.0, 0),
+            )),
+        );
+
+        let render_group_desc = MeshRenderGroupDesc::new();
+        let pass = graph_builder.add_node(
+            render_group_desc
+                .builder()
+                .into_subpass()
+                .with_color(color)
+                .with_depth_stencil(depth)
+                .into_pass(),
+        );
+
+        let present_builder = PresentNode::builder(&factory, surface, color).with_dependency(pass);
+
+        let frames = present_builder.image_count();
+
+        graph_builder.add_node(present_builder);
+
+        // HACK suggested by Frizi, just use queue 0 for everything
+        // instead of getting it from `graph.node_queue(pass)`.
+        // Since we control in our `Config` what families we have
+        // and what they have, as long as we only ever use one family
+        // (which is probably fine) then we're prooooobably okay with
+        // this.
+        // TODO: Check and see if this has immproved now
+        let queue_id = QueueId {
+            family: families.family_by_index(0).id(),
+            index: 0,
+        };
+
+        let heart_bytes =
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/data/heart.png"));
+        let texture1 = make_texture(queue_id, &mut factory, heart_bytes);
+        let object_mesh = Arc::new(make_quad_mesh(queue_id, &mut factory));
+        let draws = vec![DrawCall::new(texture1, object_mesh.clone())];
+
+        let vertex_file = concat!(env!("CARGO_MANIFEST_DIR"), "/src/data/shader.glslv");
+        let fragment_file = concat!(env!("CARGO_MANIFEST_DIR"), "/src/data/shader.glslf");
+
+        let align = factory
+            .physical()
+            .limits()
+            .min_uniform_buffer_offset_alignment;
+
+        let width = window_size.width as f32;
+        let height = window_size.height as f32;
+        let aux = Aux {
+            frames: frames as _,
+            align,
+
+            draws,
+            camera: UniformData {
+                proj: Transform3::ortho(0.0, width, height, 0.0, 1.0, 200.0),
+
+                view: Transform3::create_translation(0.0, 0.0, 10.0),
+            },
+
+            shader: load_shader_files(vertex_file, fragment_file),
+        };
+
+        let graph = graph_builder
+            .with_frames_in_flight(frames)
+            .build(&mut factory, &mut families, &aux)
+            .unwrap();
+
+        Self {
+            factory,
+            families,
+            queue_id,
+            depth,
+            color,
+            window,
+            event_loop,
+            graph,
+            aux,
+            frame_count: frames,
+            align: align,
+        }
+    }
+
+    pub fn run(&mut self) {
+        use std::time;
+        use winit::{Event, EventsLoop, WindowBuilder, WindowEvent};
+
+        let mut frames = 0u64..;
+        let mut rng = rand::thread_rng();
+
+        let mut should_close = false;
+
+        let started = time::Instant::now();
+        // TODO: Someday actually check against MAX_OBJECTS
+        while !should_close {
+            for _i in &mut frames {
+                self.factory.maintain(&mut self.families);
+                self.event_loop.poll_events(|event| match event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => should_close = true,
+                    _ => (),
+                });
+                self.graph
+                    .run(&mut self.factory, &mut self.families, &self.aux);
+                // Add another object
+                for draw_call in &mut self.aux.draws {
+                    draw_call.add_object(&mut rng, 1024.0, 768.0);
+                }
+                if should_close {
+                    break;
+                }
+            }
+        }
+        let finished = time::Instant::now();
+        let dt = finished - started;
+        let millis = dt.as_millis() as f64;
+        let fps = frames.start as f64 / (millis / 1000.0);
+        println!(
+            "{} frames over {} seconds; {} fps",
+            frames.start,
+            millis / 1000.0,
+            fps
+        );
+    }
+
+    #[cfg(not(target_os = "macos"))]
+    pub fn new_vulkan() -> GraphicsDevice<rendy::vulkan::Backend> {
+        GraphicsDevice::new()
+    }
+
+    #[cfg(target_os = "macos")]
+    pub fn new_metal() -> GraphicsDevice<rendy::metal::Backend> {
+        GraphicsDevice::new()
+    }
+
+    pub fn dispose(mut self) {
+        // TODO: This doesn't actually dispose of everything right
+        self.graph.dispose(&mut self.factory, &self.aux)
+    }
+}
+
+#[cfg(not(target_os = "macos"))]
+pub fn new_vulkan_device() -> GraphicsDevice<rendy::vulkan::Backend> {
+    GraphicsDevice::new()
+}
+
+// #[cfg(target_os = "windows")]
+// pub fn new_dx12() -> GraphicsDevice<rendy::dx12::Backend> {
+//     GraphicsDevice::new()
+// }
+
+// pub fn new_empty() -> GraphicsDevice<rendy::empty::Backend> {
+//     GraphicsDevice::new()
+// }
