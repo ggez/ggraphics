c186c9f4a052edc7d19f116d69cad2f9e2c58623
Simon Heath <icefox@dreamquest.io>
2019-10-24 13:31:26 -0400

    Update wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,67 @@
+use raw_window_handle;
 use wgpu;
 use winit;
 
 fn main() {
-    // Create device connection and such
-    let instance = wgpu::Instance::new();
-    let adapter = instance.get_adapter(&wgpu::AdapterDescriptor {
-        power_preference: wgpu::PowerPreference::Default,
+    use winit::{
+        event,
+        event_loop::{ControlFlow, EventLoop},
+    };
+
+    let event_loop = EventLoop::new();
+
+    #[cfg(not(feature = "gl"))]
+    let (_window, instance, size, surface) = {
+        use raw_window_handle::HasRawWindowHandle;
+
+        let window = winit::window::Window::new(&event_loop).unwrap();
+        let size = window.inner_size().to_physical(window.hidpi_factor());
+
+        let instance = wgpu::Instance::new();
+        let surface = instance.create_surface(window.raw_window_handle());
+
+        (window, instance, size, surface)
+    };
+
+    #[cfg(feature = "gl")]
+    let (_window, instance, size, surface) = {
+        let wb = winit::WindowBuilder::new();
+        let cb = wgpu::glutin::ContextBuilder::new().with_vsync(true);
+        let context = cb.build_windowed(wb, &event_loop).unwrap();
+
+        let size = context
+            .window()
+            .get_inner_size()
+            .unwrap()
+            .to_physical(context.window().get_hidpi_factor());
+
+        let (context, window) = unsafe { context.make_current().unwrap().split() };
+
+        let instance = wgpu::Instance::new(context);
+        let surface = instance.get_surface();
+
+        (window, instance, size, surface)
+    };
+
+    let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::LowPower,
     });
-    let mut device = adapter.create_device(&wgpu::DeviceDescriptor {
+
+    let mut device = adapter.request_device(&wgpu::DeviceDescriptor {
         extensions: wgpu::Extensions {
             anisotropic_filtering: false,
         },
+        limits: wgpu::Limits::default(),
     });
 
-    // Create rendering pipeline
-    let vert_spirv = include_bytes!("../hello_triangle.vert.spv");
-    let frag_spirv = include_bytes!("../hello_triangle.frag.spv");
+    let vs = include_bytes!("data/vert.spv");
+    let vs_module =
+        device.create_shader_module(&wgpu::read_spirv(std::io::Cursor::new(&vs[..])).unwrap());
 
-    let vertex_shader = device.create_shader_module(vert_spirv);
-    let frag_shader = device.create_shader_module(frag_spirv);
+    let fs = include_bytes!("data/frag.spv");
+    let fs_module =
+        device.create_shader_module(&wgpu::read_spirv(std::io::Cursor::new(&fs[..])).unwrap());
+
     let bind_group_layout =
         device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { bindings: &[] });
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
@@ -28,88 +71,95 @@ fn main() {
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         bind_group_layouts: &[&bind_group_layout],
     });
+
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         layout: &pipeline_layout,
-        vertex_stage: wgpu::PipelineStageDescriptor {
-            module: &vertex_shader,
+        vertex_stage: wgpu::ProgrammableStageDescriptor {
+            module: &vs_module,
             entry_point: "main",
         },
-        fragment_stage: wgpu::PipelineStageDescriptor {
-            module: &frag_shader,
+        fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+            module: &fs_module,
             entry_point: "main",
-        },
-        rasterization_state: wgpu::RasterizationStateDescriptor {
+        }),
+        rasterization_state: Some(wgpu::RasterizationStateDescriptor {
             front_face: wgpu::FrontFace::Ccw,
             cull_mode: wgpu::CullMode::None,
             depth_bias: 0,
             depth_bias_slope_scale: 0.0,
             depth_bias_clamp: 0.0,
-        },
+        }),
         primitive_topology: wgpu::PrimitiveTopology::TriangleList,
         color_states: &[wgpu::ColorStateDescriptor {
-            format: wgpu::TextureFormat::Bgra8Unorm,
-            alpha: wgpu::BlendDescriptor::REPLACE,
-            color: wgpu::BlendDescriptor::REPLACE,
-            write_mask: wgpu::ColorWriteFlags::ALL,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            color_blend: wgpu::BlendDescriptor::REPLACE,
+            alpha_blend: wgpu::BlendDescriptor::REPLACE,
+            write_mask: wgpu::ColorWrite::ALL,
         }],
         depth_stencil_state: None,
         index_format: wgpu::IndexFormat::Uint16,
         vertex_buffers: &[],
         sample_count: 1,
+        sample_mask: !0,
+        alpha_to_coverage_enabled: false,
     });
 
-    // Now create window and glue wgpu to it
-    let mut events_loop = winit::EventsLoop::new();
-    let window = winit::Window::new(&events_loop).unwrap();
-
-    let surface = instance.create_surface(&window);
-    let winit::dpi::LogicalSize { width, height } = window.get_inner_size().unwrap();
-    let swap_chain = &mut device.create_swap_chain(
+    let mut swap_chain = device.create_swap_chain(
         &surface,
         &wgpu::SwapChainDescriptor {
-            usage: wgpu::TextureUsageFlags::OUTPUT_ATTACHMENT,
-            format: wgpu::TextureFormat::Bgra8Unorm,
-            width: width as u32,
-            height: height as u32,
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width.round() as u32,
+            height: size.height.round() as u32,
+            present_mode: wgpu::PresentMode::Vsync,
         },
     );
 
-    let mut continuing = true;
-    while continuing {
-        events_loop.poll_events(|event| match event {
-            winit::Event::WindowEvent {
-                event: winit::WindowEvent::CloseRequested,
-                ..
-            } => {
-                continuing = false;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = if cfg!(feature = "metal-auto-capture") {
+            ControlFlow::Exit
+        } else {
+            ControlFlow::Poll
+        };
+        match event {
+            event::Event::WindowEvent { event, .. } => match event {
+                event::WindowEvent::KeyboardInput {
+                    input:
+                        event::KeyboardInput {
+                            virtual_keycode: Some(event::VirtualKeyCode::Escape),
+                            state: event::ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                }
+                | event::WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                _ => {}
+            },
+            event::Event::EventsCleared => {
+                let frame = swap_chain.get_next_texture();
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                            attachment: &frame.view,
+                            resolve_target: None,
+                            load_op: wgpu::LoadOp::Clear,
+                            store_op: wgpu::StoreOp::Store,
+                            clear_color: wgpu::Color::GREEN,
+                        }],
+                        depth_stencil_attachment: None,
+                    });
+                    rpass.set_pipeline(&render_pipeline);
+                    rpass.set_bind_group(0, &bind_group, &[]);
+                    rpass.draw(0..3, 0..1);
+                }
+
+                device.get_queue().submit(&[encoder.finish()]);
             }
             _ => (),
-        });
-        // present() is called automatically when next_texture is dropped.
-        let next_texture = swap_chain.get_next_texture();
-        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            // lulz
-            todo: 0,
-        });
-        let color_attachments = wgpu::RenderPassColorAttachmentDescriptor {
-            attachment: &next_texture.view,
-            load_op: wgpu::LoadOp::Clear,
-            store_op: wgpu::StoreOp::Store,
-            clear_color: wgpu::Color::BLUE,
-        };
-        {
-            // The pass is finished automatically when rpass is drawn
-            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[color_attachments],
-                depth_stencil_attachment: None,
-            });
-
-            rpass.set_pipeline(&render_pipeline);
-            rpass.set_bind_group(0, &bind_group);
-            rpass.draw(0..3, 0..1);
         }
-        let cmd_buffer = encoder.finish();
-        let mut queue = device.get_queue();
-        queue.submit(&[cmd_buffer]);
-    }
+    });
 }
