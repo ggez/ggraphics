490e63b51dbe56868bdeceee0c9f5eb9eea486a8
Simon Heath <icefox@dreamquest.io>
2019-11-05 21:10:28 -0500

    wat

---
+++ wat.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cargo build --target wasm32-unknown-unknown
+wasm-bindgen target/wasm32-unknown-unknown/debug/ggraphics.wasm --out-dir generated --no-modules
+rsync -av generated/ roc.alopex.li:htdocs/temp/g6
+
