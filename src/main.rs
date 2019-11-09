7407f27fa5e0cc740725e58c925d7e88f3e29679
Simon Heath <icefoxen@gmail.com>
2019-11-09 13:12:33 -0500

    AUGH

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,17 @@ impl GlContext {
             let mut pipeline = QuadPipeline::new(program);
             let texture = Self::create_texture(&gl);
             let mut drawcall = QuadDrawCall::new(&gl, texture, SamplerSpec {}, &pipeline.program);
+            
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            drawcall.add(QuadData { offset: [0.0, 0.0] });
+            /*
             drawcall.add(QuadData {
                 offset: [-0.5, 0.5],
             });
@@ -152,6 +163,7 @@ impl GlContext {
             drawcall.add(QuadData { offset: [0.0, 0.0] });
             drawcall.add(QuadData { offset: [0.0, 0.0] });
             drawcall.add(QuadData { offset: [0.0, 0.0] });
+            */
             pipeline.drawcalls.push(drawcall);
             GlContext {
                 gl,
@@ -252,7 +264,8 @@ impl QuadDrawCall {
 
             // TODO: https://github.com/grovesNL/glow/issues/54
             let offset_attrib = gl.get_attrib_location(*shader, "offset") as u32;
-            gl.vertex_attrib_pointer_f32(offset_attrib, 2, glow::FLOAT, false, 0, 0);
+            // gl.vertex_attrib_pointer_f32(offset_attrib, 2, glow::FLOAT, false, (2 * mem::size_of::<f32>()) as i32, 0);
+            gl.vertex_attrib_pointer_f32(offset_attrib, 2, glow::FLOAT, false, 8, 0);
             // TODO: Double-check if 3 is correct
             //gl.vertex_attrib_divisor(offset_attrib, 3);
             gl.enable_vertex_attrib_array(offset_attrib);
@@ -274,6 +287,7 @@ impl QuadDrawCall {
 
     /// Upload the array of instances to our VBO
     unsafe fn upload_instances(&self, gl: &Context) {
+        gl.bind_vertex_array(Some(self.vao));
         gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vbo));
         // TODO: Invalidate buffer on change instead of refilling it all the time
         // TODO: Make instance data cast not suck
@@ -292,6 +306,8 @@ impl QuadDrawCall {
         let num_bytes = f32_buffer.len() * mem::size_of::<f32>();
         let bytes_ptr = f32_buffer.as_ptr() as *const u8;
         let bytes_slice = std::slice::from_raw_parts(bytes_ptr, num_bytes);
+        println!("{:?}", bytes_slice);
+        println!("Num instances: {}, num bytes: {}", self.instances.len(), num_bytes);
         // TODO: Make usage sensible
         gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, bytes_slice, glow::STREAM_DRAW);
     }
@@ -306,6 +322,7 @@ impl QuadDrawCall {
         println!("Drawing {} verts", num_vertices);
         //gl.draw_arrays(glow::TRIANGLES, 0, 3);
         gl.bind_vertex_array(Some(self.vao));
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vbo)); 
         gl.draw_arrays(glow::TRIANGLES, 0, num_vertices as i32);
     }
 
@@ -435,7 +452,7 @@ fn run_glutin() {
             use glutin::event_loop::ControlFlow;
 
             event_loop.run(move |event, _, control_flow| {
-                *control_flow = ControlFlow::Wait;
+                *control_flow = ControlFlow::Poll;
                 match event {
                     Event::LoopDestroyed => {
                         info!("Event::LoopDestroyed!");
